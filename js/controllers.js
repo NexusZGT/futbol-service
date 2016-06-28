074aacae2ed8de78806ed80aefb9c9a6769c611e
giancarlosio <giancarlos.isasi@gmail.com>
2016-06-28 01:10:48 -0500

    cros origin

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,8 @@ var futbolCtrl = angular.module("futbolCtrl",[]);
 
 futbolCtrl.controller("listTournamentsCtrl", ["$scope","$http",function($scope,$http) {
 
-		var url= "http://api.football-api.com/2.0/competitions?Authorization=565eaa22251f932b9f000001d50aaf0b55c7477c5ffcdbaf113ebbda";
+		//var url= "http://api.football-api.com/2.0/competitions?Authorization=565eaa22251f932b9f000001d50aaf0b55c7477c5ffcdbaf113ebbda";
+		var url = 'http://api.football-data.org/v1/soccerseasons'
 		$http.get(url).success(function(data){
 			$scope.tournaments = data;
 		});
