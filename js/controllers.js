cced5d8f1d6b086a44ce391c8b3e054ca09fad8b
giancarlosio <giancarlos.isasi@gmail.com>
2016-06-28 01:01:17 -0500

    FIx api

--- js/controllers.js
+++ js/controllers.js
@@ -3,17 +3,13 @@ var futbolCtrl = angular.module("futbolCtrl",[]);
 futbolCtrl.controller("listTournamentsCtrl", ["$scope","$http",function($scope,$http) {
 
 		var url= "http://api.football-api.com/2.0/competitions?Authorization=565eaa22251f932b9f000001d50aaf0b55c7477c5ffcdbaf113ebbda";
-		$http({
-			method: 'GET',
-			url:'url',
-					}
-		).success(function(data){
+		$http.get(url).success(function(data){
 			$scope.tournaments = data;
 		});
 }]);
 
 futbolCtrl.controller("detalleTournamentCtrl",["$scope", "$http","$routeParams",function($scope,$http,$routeParams){
-		var urlDetalles = "http://api.football-data.org/v1/soccerseasons/"+ $routeParams.tournamentID;
+		var urlDetalles = "http://api.football-api.com/2.0/competitions/"+ $routeParams.tournamentID+ '?Authorization=565eaa22251f932b9f000001d50aaf0b55c7477c5ffcdbaf113ebbda';
 		$http.get(urlDetalles).success(function(datos){
 			$scope.detalleTournament = datos;
 		});
